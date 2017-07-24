517c7e89a42b38b3109a7f4ab19d98b1b45bda95
Megalawls <daniel.russell@jrweb.org.uk>
2017-07-24 17:08:56 +0100

    Added global setting override to redirect to my 404 page, should handle any errors regarding incorrect URL entering

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,9 @@ import play.api.mvc._
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index(24859076))
   }
-
+  def myPage = Action {
+    Ok(views.html.myPage("This is my custom page"))
+  }
 }
--- build.sbt
+++ build.sbt
@@ -11,5 +11,3 @@ scalaVersion := "2.11.11"
 libraryDependencies ++= Seq( jdbc , cache , ws , specs2 % Test )
 
 unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
-
-      

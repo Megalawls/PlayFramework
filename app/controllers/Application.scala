afe91588731898f617f58062cc610b24fc507a0f
Megalawls <daniel.russell@jrweb.org.uk>
2017-07-25 20:02:09 +0100

    Added sessions and a load of pages, also added a decent amount of CSS

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,10 +5,29 @@ import play.api.mvc._
 
 class Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index(24859076))
+  def index = Action {request =>
+    val last = request.session.get("lastPage").getOrElse("no previous page")
+    Ok(views.html.index(24859076, last)).withSession("lastPage" -> "home")
   }
-  def myPage = Action {
-    Ok(views.html.myPage("This is my custom page"))
+
+  def redirectIndex = Action {
+    Redirect(routes.Application.index())
   }
+
+  def myPage = Action {request =>
+    val last = request.session.get("lastPage").getOrElse("no previous page")
+    Ok(views.html.myPage("This is my custom page", last)).withSession("lastPage" -> "myPage")
+  }
+
+  def dynamic(name: String) = Action {request =>
+    val last = request.session.get("lastPage").getOrElse("no previous page")
+    Ok(views.html.dynamic(name, last)).withSession("lastPage" -> "dynamic")
+  }
+
+  def option(optionalParam: Option[String]) = Action {request =>
+    val last = request.session.get("lastPage").getOrElse("no previous page")
+    Ok(views.html.options(optionalParam, last)).withSession("lastPage" -> "option")
+  }
+
+  def gitGud = TODO
 }
